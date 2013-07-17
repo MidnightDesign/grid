5b2efa639bc723c5706cca8934d1769bd38fdc81
Rudolph Gottesheim <r.gottesheim@loot.at>
2013-07-17 11:20:28 +0200

    Fix cell assignment

--- lib/Midnight/Grid/Renderer/Html/RowRenderer.php
+++ lib/Midnight/Grid/Renderer/Html/RowRenderer.php
@@ -2,6 +2,7 @@
 
 namespace Midnight\Grid\Renderer\Html;
 
+use Midnight\Grid\Cell\Cell;
 use Midnight\Grid\Row\RowInterface;
 
 class RowRenderer extends AbstractGridElement
@@ -15,16 +16,20 @@ class RowRenderer extends AbstractGridElement
     public function render(RowInterface $row)
     {
         $this->setRow($row);
-        return $this->getOpenTag().$this->getContents().$this->getCloseTag();
+        return $this->getOpenTag() . $this->getContents() . $this->getCloseTag();
     }
 
     private function getContents()
     {
         $r = array();
-        $cells = $this->getRow()->getCells();
+        $columns = $this->getRow()->getGrid()->getColumns();
         $cellRenderer = new CellRenderer();
-        foreach($cells as $cell) {
-            $r[] = $cellRenderer->render($cell);
+        foreach ($columns as $column) {
+            try {
+                $r[] = $cellRenderer->render($this->getRow()->getCells()->offsetGet($column));
+            } catch (\Exception $e) {
+                $r[] = $cellRenderer->render(new Cell());
+            }
         }
         return join(PHP_EOL, $r);
     }
@@ -34,6 +39,9 @@ class RowRenderer extends AbstractGridElement
         $this->row = $row;
     }
 
+    /**
+     * @return RowInterface
+     */
     private function getRow()
     {
         return $this->row;
--- lib/Midnight/Grid/Row/RowInterface.php
+++ lib/Midnight/Grid/Row/RowInterface.php
@@ -2,11 +2,25 @@
 
 namespace Midnight\Grid\Row;
 
+use Midnight\Grid\Cell\CellInterface;
 use Midnight\Grid\GridInterface;
 
-interface RowInterface {
+interface RowInterface
+{
 
+    /**
+     * @param GridInterface $grid
+     * @return mixed
+     */
     public function setGrid(GridInterface $grid);
 
+    /**
+     * @return GridInterface
+     */
+    public function getGrid();
+
+    /**
+     * @return \SplObjectStorage|CellInterface[]
+     */
     public function getCells();
 }
