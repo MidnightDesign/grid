8e7ae4ad2cdf3396b649225260df5526cc8038f3
Rudolph Gottesheim <r.gottesheim@loot.at>
2013-07-17 10:11:02 +0200

    Implement adding cells by column name

--- lib/Midnight/Grid/Cell/Cell.php
+++ lib/Midnight/Grid/Cell/Cell.php
@@ -10,18 +10,23 @@ class Cell implements CellInterface
     private $row;
     private $value;
 
+    function __construct($value = null)
+    {
+        $this->value = $value;
+    }
+
     public function setRow(RowInterface $row)
     {
         $this->row = $row;
     }
 
-    public function setValue($value)
-    {
-        $this->value = $value;
-    }
-
     public function getValue()
     {
         return $this->value;
+    }
+
+    public function setValue($value)
+    {
+        $this->value = $value;
     }
 }
--- lib/Midnight/Grid/Row/Row.php
+++ lib/Midnight/Grid/Row/Row.php
@@ -29,10 +29,46 @@ class Row implements RowInterface
         $this->grid = $grid;
     }
 
-    public function setCell(CellInterface $cell, ColumnInterface $column)
+    /**
+     * @TODO This seems overly complex. There has to be a cleaner way.
+     * @param CellInterface $cell
+     * @param ColumnInterface|string $column
+     */
+    public function setCell(CellInterface $cell, $column)
     {
+        if (is_string($column)) {
+            $grid = $this->getGrid();
+            if(is_null($grid)) {
+                throw new \LogicException('The row must be added to a table before you can assign cells by column name.');
+            }
+            $columns = $grid->getColumns();
+            $found = null;
+            $columns->rewind();
+            while (is_null($found)) {
+                $current = $columns->current();
+                if (!$current) {
+                    throw new \InvalidArgumentException('Could not find a column named "' . $column . '".');
+                }
+                if ($current->getName() === $column) {
+                    $found = $current;
+                }
+                $columns->next();
+            }
+            $column = $found;
+        }
+        if (!$column instanceof ColumnInterface) {
+            throw new \InvalidArgumentException('The second parameter must implement Midnight\Grid\Column\ColumnInterface.');
+        }
         $this->getCells()->attach($cell, $column);
         $cell->setRow($this);
+    }
+
+    /**
+     * @return \Midnight\Grid\GridInterface
+     */
+    public function getGrid()
+    {
+        return $this->grid;
     }
 
     public function getCells()
